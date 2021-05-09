3dc8d160363ccd1963484c5fcd46353773737b41
Kostandina Qirjazi <k.qirjazi1@gmail.com>
2021-05-09 17:58:21 +0200

    Add files via upload

---
+++ JavaScript.js
@@ -0,0 +1,33 @@
+﻿function addItem() {
+    var ul = document.getElementById("list1");
+    var movie = document.getElementById("movie");
+    var lii= document.createElement("li");
+    lii.setAttribute('id', movie.value);
+    lii.appendChild(document.createTextNode(movie.value));
+    ul.appendChild(lii);
+}
+
+function removeItem() {
+    var ul = document.getElementById("list1");
+    var movie = document.getElementById("movie");
+    var item = document.getElementById(movie.value);
+    ul.removeChild(item);
+}
+localStorage.setItem('key', 'value');
+let variable = loaclStorage.getItem('key');
+localStorage.setItem("store", JSON.stringify(li));
+
+let data = localStorage.getItem("store");
+if (data) {
+    li = JSON.parse(data);
+    loadToDo(li);
+    id = li.length;
+} else {
+    li = [];
+    
+}
+function loadToDo(array) {
+    array.forEach(function (item) {
+        addToDo(item.name, item.id, item.done, item.trash);
+    });
+}
